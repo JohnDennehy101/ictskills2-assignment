d6dfcccb1d15b55d510f8fa0a7eb6df974a5ba53
JohnDennehy <johndennehy101@gmail.com>
2021-07-25 13:55:28 +0100

    Split out credits table functionality into separate component.

---
+++ src/components/castMemberCredits/index.js
@@ -0,0 +1,231 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import img from "../../images/film-poster-placeholder.png";
+import Button from "@material-ui/core/Button";
+
+export default function CastMemberCredits({
+  page,
+  rowsPerPage,
+  rows,
+  handleChangePage,
+  handleChangeRowsPerPage,
+}) {
+  const useStyles = makeStyles((theme) => ({
+    title: {
+      textAlign: "center",
+      margin: "20px 10px",
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+  }));
+
+  const columns = [
+    { id: "poster", label: "Poster", minWidth: 170 },
+    { id: "title", label: "Title", minWidth: 170 },
+    { id: "character", label: "Character", minWidth: 100 },
+    {
+      id: "overview",
+      label: "Summary",
+      minWidth: 220,
+      align: "left",
+    },
+    {
+      id: "release_date",
+      label: "Release Date",
+      minWidth: 170,
+      align: "left",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "average_rating",
+      label: "Average Rating",
+      minWidth: 170,
+      align: "left",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "type",
+      label: "Media Type",
+      minWidth: 170,
+      align: "left",
+    },
+    {
+      id: "id",
+      label: "id",
+      minWidth: 0,
+      align: "left",
+    },
+  ];
+
+  // function createData(
+  //   poster,
+  //   title,
+  //   character,
+  //   overview,
+  //   release_date,
+  //   average_rating,
+  //   type,
+  //   id
+  // ) {
+  //   return {
+  //     poster,
+  //     title,
+  //     character,
+  //     overview,
+  //     release_date,
+  //     average_rating,
+  //     type,
+  //     id,
+  //   };
+  // }
+
+  //  data.cast.forEach((item) => {
+  //     let title = "";
+  //     if ("name" in item) {
+  //       title = item["name"];
+  //     } else {
+  //       title = item["title"];
+  //     }
+  //     rows.push(
+  //       createData(
+  //         `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+  //         title,
+  //         item.character,
+  //         item.overview,
+  //         item.release_date,
+  //         item.vote_average,
+  //         item.media_type,
+  //         item.id
+  //       )
+  //     );
+  //   });
+
+  //    const handleChangePage = (event, newPage) => {
+  //     setPage(newPage);
+  //   };
+
+  //   const handleChangeRowsPerPage = (event) => {
+  //     setRowsPerPage(+event.target.value);
+  //     setPage(0);
+  //   };
+
+  console.log(rows);
+
+  const classes = useStyles();
+  return (
+    <>
+      <Typography className={classes.title} variant="h3" gutterBottom>
+        Appears In
+      </Typography>
+      <Grid item xs={12}>
+        <Paper className={classes.root}>
+          <TableContainer className={classes.container}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => {
+                    return (
+                      <TableRow
+                        hover
+                        role="checkbox"
+                        tabIndex={-1}
+                        key={row.code}
+                      >
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          let valueImageCheck = undefined;
+                          let imageNullCheck = undefined;
+                          let buttonCheck = undefined;
+
+                          if (column.id === "poster") {
+                            valueImageCheck = true;
+                            if (value.includes("null")) {
+                              imageNullCheck = true;
+                            }
+                          } else if (column.id === "id") {
+                            valueImageCheck = true;
+                            buttonCheck = true;
+                            imageNullCheck = true;
+                          }
+
+                          return (
+                            <>
+                              {valueImageCheck === undefined ? (
+                                <TableCell key={column.id} align={column.align}>
+                                  {column.format && typeof value === "number"
+                                    ? column.format(value)
+                                    : value}
+                                </TableCell>
+                              ) : imageNullCheck === undefined ? (
+                                <TableCell>
+                                  <img
+                                    width="200px"
+                                    height="125px"
+                                    src={`https://image.tmdb.org/t/p/w500${value}`}
+                                  />
+                                </TableCell>
+                              ) : buttonCheck === undefined ? (
+                                <TableCell>
+                                  <img width="200px" height="125px" src={img} />
+                                </TableCell>
+                              ) : (
+                                <TableCell>
+                                  <Button
+                                    variant="contained"
+                                    color="primary"
+                                    href={`/movies/${value}`}
+                                  >
+                                    More Info
+                                  </Button>
+                                </TableCell>
+                              )}
+                            </>
+                          );
+                        })}
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </Grid>
+    </>
+  );
+}
--- src/pages/castMemberInfo.js
+++ src/pages/castMemberInfo.js
@@ -3,28 +3,17 @@ import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
 import { getPersonCredits, getPersonDetail } from "../api/tmdb-api";
 import Container from "@material-ui/core/Container";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
 import PersonPinIcon from "@material-ui/icons/PersonPin";
 import HomeIcon from "@material-ui/icons/Home";
 import FavoriteIcon from "@material-ui/icons/Favorite";
-import img from "../images/film-poster-placeholder.png";
-import Button from "@material-ui/core/Button";
 import Fab from "@material-ui/core/Fab";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-
 import CastMemberAvatar from "../components/castMemberAvatar";
 import CastMemberIndividualInfo from "../components/castMemberIndividualInfo";
 import CastMemberBiography from "../components/castMemberBiography";
+import CastMemberCredits from "../components/castMemberCredits";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -53,74 +42,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const columns = [
-  { id: "poster", label: "Poster", minWidth: 170 },
-  { id: "title", label: "Title", minWidth: 170 },
-  { id: "character", label: "Character", minWidth: 100 },
-  {
-    id: "overview",
-    label: "Summary",
-    minWidth: 220,
-    align: "left",
-  },
-  {
-    id: "release_date",
-    label: "Release Date",
-    minWidth: 170,
-    align: "left",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "average_rating",
-    label: "Average Rating",
-    minWidth: 170,
-    align: "left",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "type",
-    label: "Media Type",
-    minWidth: 170,
-    align: "left",
-  },
-  {
-    id: "id",
-    label: "id",
-    minWidth: 0,
-    align: "left",
-  },
-];
-
-function createData(
-  poster,
-  title,
-  character,
-  overview,
-  release_date,
-  average_rating,
-  type,
-  id
-) {
-  return {
-    poster,
-    title,
-    character,
-    overview,
-    release_date,
-    average_rating,
-    type,
-    id,
-  };
-}
-
 const CastMemberInfoPage = (props) => {
   let rows = [];
-  let age;
-  let currentDate = new Date();
 
-  const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const classes = useStyles();
   const { data, error, isLoading, isError } = useQuery(
     ["personCredits", props.match.params.id],
     () => getPersonCredits(props.match.params.id)
@@ -136,6 +63,28 @@ const CastMemberInfoPage = (props) => {
 
   if (isError) {
     return <h1>{error.message}</h1>;
+  }
+
+  function createData(
+    poster,
+    title,
+    character,
+    overview,
+    release_date,
+    average_rating,
+    type,
+    id
+  ) {
+    return {
+      poster,
+      title,
+      character,
+      overview,
+      release_date,
+      average_rating,
+      type,
+      id,
+    };
   }
 
   data.cast.forEach((item) => {
@@ -158,11 +107,6 @@ const CastMemberInfoPage = (props) => {
       )
     );
   });
-
-  if (!personDetailLoading) {
-    age =
-      currentDate.getFullYear() - new Date(personDetail.birthday).getFullYear();
-  }
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -198,117 +142,18 @@ const CastMemberInfoPage = (props) => {
               />
 
               <CastMemberBiography biography={personDetail.biography} />
-
-              <Typography className={classes.title} variant="h3" gutterBottom>
-                Appears In
-              </Typography>
-              <Grid item xs={12}>
-                <Paper className={classes.root}>
-                  <TableContainer className={classes.container}>
-                    <Table stickyHeader aria-label="sticky table">
-                      <TableHead>
-                        <TableRow>
-                          {columns.map((column) => (
-                            <TableCell
-                              key={column.id}
-                              align={column.align}
-                              style={{ minWidth: column.minWidth }}
-                            >
-                              {column.label}
-                            </TableCell>
-                          ))}
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {rows
-                          .slice(
-                            page * rowsPerPage,
-                            page * rowsPerPage + rowsPerPage
-                          )
-                          .map((row) => {
-                            return (
-                              <TableRow
-                                hover
-                                role="checkbox"
-                                tabIndex={-1}
-                                key={row.code}
-                              >
-                                {columns.map((column) => {
-                                  const value = row[column.id];
-                                  let valueImageCheck = undefined;
-                                  let imageNullCheck = undefined;
-                                  let buttonCheck = undefined;
-
-                                  if (column.id === "poster") {
-                                    valueImageCheck = true;
-                                    if (value.includes("null")) {
-                                      imageNullCheck = true;
-                                    }
-                                  } else if (column.id === "id") {
-                                    valueImageCheck = true;
-                                    buttonCheck = true;
-                                    imageNullCheck = true;
-                                  }
-
-                                  return (
-                                    <>
-                                      {valueImageCheck === undefined ? (
-                                        <TableCell
-                                          key={column.id}
-                                          align={column.align}
-                                        >
-                                          {column.format &&
-                                          typeof value === "number"
-                                            ? column.format(value)
-                                            : value}
-                                        </TableCell>
-                                      ) : imageNullCheck === undefined ? (
-                                        <TableCell>
-                                          <img
-                                            width="200px"
-                                            height="125px"
-                                            src={`https://image.tmdb.org/t/p/w500${value}`}
-                                          />
-                                        </TableCell>
-                                      ) : buttonCheck === undefined ? (
-                                        <TableCell>
-                                          <img
-                                            width="200px"
-                                            height="125px"
-                                            src={img}
-                                          />
-                                        </TableCell>
-                                      ) : (
-                                        <TableCell>
-                                          <Button
-                                            variant="contained"
-                                            color="primary"
-                                            href={`/movies/${value}`}
-                                          >
-                                            More Info
-                                          </Button>
-                                        </TableCell>
-                                      )}
-                                    </>
-                                  );
-                                })}
-                              </TableRow>
-                            );
-                          })}
-                      </TableBody>
-                    </Table>
-                  </TableContainer>
-                  <TablePagination
-                    rowsPerPageOptions={[10, 25, 100]}
-                    component="div"
-                    count={rows.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                  />
-                </Paper>
-              </Grid>
+              {rows.length > 0 ? (
+                <CastMemberCredits
+                  data={data}
+                  page={page}
+                  rowsPerPage={rowsPerPage}
+                  rows={rows}
+                  handleChangePage={handleChangePage}
+                  handleChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+              ) : (
+                <Spinner />
+              )}
             </Grid>
           </div>
           <Fab
